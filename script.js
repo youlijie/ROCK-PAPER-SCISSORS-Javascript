4dbe5139bb86a14d9a5489da07141e02aa387d36
youlijie <63795226+youlijie@users.noreply.github.com>
2022-06-10 17:10:06 +0200

    Logic Done

--- script.js
+++ script.js
@@ -4,59 +4,89 @@ const computerPlay = () => {
     return choice
 }
 
+const scorePlayer = document.querySelector("#scorePlayer")
+const scoreComputer = document.querySelector("#scoreComputer")
+const result = document.querySelector("#result")
+const round = document.querySelector("#round")
+let playerPoints = 0
+let computerPoints = 0
 
 
-const singleRound = (playerSelection, computerSelection) => {
-    if (playerSelection === computerSelection) {
-        return "It's a tie!"
-    }
-    if (playerSelection === "rock") {
-        if (computerSelection === "paper") {
-            return "You lose! Paper beats Rock."
-        } else {
-            return "You win! Rock beats Scissors."
-        }
-    }
-    if (playerSelection === "paper") {
-        if (computerSelection === "scissors") {
-            return "You lose! Scissors beats Paper."
-        } else {
-            return "You win! Paper beats Rock."
-        }
-    }
-    if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            return "You lose! Rock beats Scissors."
-        } else {
-            return "You win! Scissors beats Paper."
-        }
-    }
+
+
+const playerChoice = () => {
+    const buttons = document.querySelectorAll("button")
+
+    buttons.forEach(button => {
+        button.addEventListener("click", e => {
+            const playerSelection = e.target.value
+            console.log(playerSelection)
+            const computerSelection = computerPlay()
+            console.log(computerSelection);
+
+            if (playerSelection === "rock") {
+                if (computerSelection === "paper") {
+                    result.textContent="You lose! Paper beats Rock."
+                } else {
+                    result.textContent="You win! Rock beats Scissors."
+                }
+            }
+            if (playerSelection === "paper") {
+                if (computerSelection === "scissors") {
+                    result.textContent="You lose! Scissors beats Paper."
+                } else {
+                    result.textContent="You win! Paper beats Rock."
+                }
+            }
+            if (playerSelection === "scissors") {
+                if (computerSelection === "rock") {
+                    result.textContent="You lose! Rock beats Scissors."
+                } else {
+                    result.textContent="You win! Scissors beats Paper."
+                }
+            }
+
+            if (playerSelection === computerSelection) {
+                result.textContent="It's a tie!"
+            }
+
+                if(result.textContent.includes("win")) {
+                    playerPoints++
+                    console.log(`You have ${playerPoints} points and the computer has ${computerPoints} points`)
+                } else if(result.textContent.includes("lose")) {
+                    computerPoints++
+                    console.log(`You have ${playerPoints} points and the computer has ${computerPoints} points`)
+                } else {
+                    console.log(`Tie, You have ${playerPoints} points and the computer has ${computerPoints} points`)
+                }
+
+                scorePlayer.textContent = playerPoints
+                scoreComputer.textContent = computerPoints
+
+                if(playerPoints === 5) {
+                    result.textContent = "You win the game"
+                    playerPoints = 0
+                    computerPoints = 0
+                } else if(computerPoints === 5) {
+                    result.textContent = "You lost the game"
+                    playerPoints = 0
+                    computerPoints = 0
+                }
+        })
+    })
 }
 
-const game = () => {
-    let playerPoints = 0
-    let computerPoints = 0
-    for(let partyNumber = 0; partyNumber < 5; partyNumber++) {
-        const computerSelection = computerPlay()
-        const playerSelection = prompt("Rock, Paper or Scissors ?").toLowerCase()
-        const result = singleRound(playerSelection, computerSelection)
-        if(result.includes("win")) {
-            playerPoints++
-            console.log(`You have ${playerPoints} points and the computer has ${computerPoints} points`)
-        } else if(result.includes("lose")) {
-            computerPoints++
-            console.log(`You have ${playerPoints} points and the computer has ${computerPoints} points`)
-        } else {
-            console.log(`Tie, You have ${playerPoints} points and the computer has ${computerPoints} points`)
-        }
-    }
-    if(playerPoints > computerPoints) {
-        console.log("You win the game!") 
-    } else if(playerPoints < computerPoints) {
-        console.log("You lose the game!")
-    } else {
-        console.log("It's a tie!")
-    }
-}
+playerChoice()
 
-game()
+
+// const game = () => {
+
+
+//     if(playerPoints > computerPoints) {
+//         console.log("You win the game!") 
+//     } else if(playerPoints < computerPoints) {
+//         console.log("You lose the game!")
+//     } else {
+//         console.log("It's a tie!")
+//     }
+// }
